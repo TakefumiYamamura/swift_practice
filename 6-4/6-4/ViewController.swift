eb97477bb158d2a2c59c7c076089ed6fdbff8d37
TakeumiYamamura <yama1223xxx@gmail.com>
2015-06-22 10:38:08 +0900

    6-4 first commit

---
+++ 6-4/6-4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  6-4
+//
+//  Created by TakefumiYamamura on 2015/06/22.
+//  Copyright (c) 2015年 山村　武史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
